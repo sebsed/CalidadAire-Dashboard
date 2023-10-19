42ce1c1c13de2ea6dc1f25048f55453e7346a4dc
Sebastián Castañeda <snsedano@gmail.com>
2023-10-19 14:26:31 -0600

    Separación de archivos de conexión a BD

--- db-analisis.php
+++ db-analisis.php
@@ -17,13 +17,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo '<script type="text/javascript"> window.onload = function () { alert("La fecha inferior no puede ser mayor a la superior. Por favor selecciona un rango correcto."); } </script>'; 
         }
         else{
-            // Cuando se necesita obtener gráficas de pestaña de análisis
+            // Hacer query de las métricas seleccionadas
             if(isset($_POST['met_1']) && isset($_POST['met_2'])){
                 $query = "SELECT Timestamp, ".$_POST['met_1'].", ".$_POST['met_2']." FROM report WHERE Timestamp BETWEEN :fecha_inferior AND :fecha_superior";
-            }
-            else{
-                // Consulta SQL para obtener los datos dentro del rango de fechas
-                $query = "SELECT Timestamp, PM25, PM10, Ozone, Carbon_Mono, Temperature, Humidity FROM report WHERE Timestamp BETWEEN :fecha_inferior AND :fecha_superior";
             }
 
             try {
@@ -44,22 +40,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-// Inicializar arreglos para cada métrica
+// Inicializar arreglos para métricas
 $timeData = [];
-$pm25Data = [];
-$pm10Data = [];
-$ozoneData = [];
-$carbonData = [];
-$tempData = [];
-$humData = [];
-
-// Inicializar arreglos para sección de análisis
 $met1Data = [];
 $met2Data = [];
 $met1Title = $_POST['met_1'];
 $met2Title = $_POST['met_2'];
 
-// Cuando esté en la parte de análisis
 if(isset($_POST['met_1']) && isset($_POST['met_2'])){
     // Si $data no está vacío
     if (isset($data) && !empty($data)) {
@@ -70,20 +57,25 @@ if(isset($_POST['met_1']) && isset($_POST['met_2'])){
         }
     }
 }
-else{
-    // Cuando esté en la parte de histórico
-    // Si $data no está vacío
-    if (isset($data) && !empty($data)) {
-        foreach ($data as $row) {
-            $timeData[] = $row['Timestamp'];
-            $pm25Data[] = $row['PM25'];
-            $pm10Data[] = $row['PM10'];
-            $ozoneData[] = $row['Ozone'];
-            $carbonData[] = $row['Carbon_Mono'];
-            $tempData[] = $row['Temperature'];
-            $humData[] = $row['Humidity'];
-        }
-    }
-}
+
+// Normalización de datos para métrica 1
+$min1 = min($met1Data);
+$max1 = max($met1Data);
+$normalizedData1 = array_map(function ($value) use ($min1, $max1) {
+    return ($value - $min1) / ($max1 - $min1);
+}, $met1Data);
+
+// Normalización de datos para métrica 2
+$min2 = min($met2Data);
+$max2 = max($met2Data);
+$normalizedData2 = array_map(function ($value) use ($min2, $max2) {
+    return ($value - $min2) / ($max2 - $min2);
+}, $met2Data);
+
+// Pasar nombres de etiquetas
+echo '<script>';
+echo 'var met1Title = ' . json_encode($met1Title) . ';';
+echo 'var met2Title = ' . json_encode($met2Title) . ';';
+echo '</script>';
 
 ?>
